bfe6a0fb9a1101bfdb0f324b02f34540c4c4e6fb
Jeffrey Burns <jburns425@gmail.com>
2019-08-02 11:17:39 -0400

    Uploading the the updated html and jquery

---
+++ views/layouts/jquery.js
@@ -0,0 +1,72 @@
+var key = "3d0ca4168d5bfa646333274dcc83da26";
+var userInput = [];
+var terms = "";
+
+function searchIngredient() {
+  var QueryURL =
+    "https://www.food2fork.com/api/search?key=" +
+    key +
+    "&q=" +
+    terms +
+    "&page=1";
+  $.ajax({
+    url: QueryURL,
+    method: "GET"
+  }).then(function(response) {
+    var test = JSON.parse(response);
+    for (let i = 0; i < 8; i++) {
+      var recipeDiv = $("<div class='card'>");
+      var title = test.recipes[i].title;
+      var sourceURL = test.recipes[i].source_url;
+      var imgURL = test.recipes[i].image_url;
+      var publisher = test.recipes[i].publisher;
+
+      var titleOne = $("<h5 class='card-title'>").text(title);
+      var pubOne = $("<p class='card-text'>").text("Publisher: " + publisher);
+      var sourceOne = $("<a>").attr("href", sourceURL);
+      sourceOne.text("Source");
+      var imageOne = $("<img class='card-img-top'>").attr("src", imgURL);
+      var buttoneOne = $("<i class = 'fa fa-thumbs-up '>").addClass(
+        "btn btn-primary"
+      );
+      var buttonTwo = $("<i class = 'fa fa-thumbtack'>").addClass(
+        "btn btn-danger"
+      );
+      // <a href="#" class="btn btn-primary" id = "thumbs-up"><i class="fa fa-thumbs-up"></i></a>
+      recipeDiv.append(titleOne);
+      recipeDiv.append(sourceOne);
+      recipeDiv.append(pubOne);
+      recipeDiv.append(imageOne);
+      recipeDiv.append(buttoneOne);
+      recipeDiv.append(buttonTwo);
+
+      var check = $("#result-box-row").append(recipeDiv);
+      $("#result-box").append(check);
+    }
+  });
+}
+
+$("#add-ingredient").on("click", function() {
+  event.preventDefault();
+
+  userInput.push($("#ingredient-one").val());
+  userInput.push($("#ingredient-two").val());
+  userInput.push($("#ingredient-three").val());
+  userInput.push($("#ingredient-four").val());
+  userInput.push($("#ingredient-five").val());
+  for (let i = 0; i < userInput.length; i++) {
+    terms += userInput[i];
+    terms += ", ";
+  }
+
+  searchIngredient();
+
+  $("#ingredient-one").val("");
+  $("#ingredient-two").val("");
+  $("#ingredient-three").val("");
+  $("#ingredient-four").val("");
+  $("#ingredient-five").val("");
+
+  userInput = [];
+  terms = "";
+});
