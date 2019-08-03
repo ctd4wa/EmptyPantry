4d0a487b515df769fcb1fdcbab1d688a1ec7b0ea
Jeffrey Burns <jburns425@gmail.com>
2019-08-03 17:11:40 -0400

    Updating jquery to dynamically add buttons

--- views/layouts/jquery.js
+++ views/layouts/jquery.js
@@ -24,25 +24,57 @@ function searchIngredient() {
       var sourceURL = test.recipes[i].source_url;
       var imgURL = test.recipes[i].image_url;
       var publisher = test.recipes[i].publisher;
+      var recipeId = test.recipes[i].recipe_id;
 
       var titleOne = $("<h5 class='card-title'>").text(title);
       var pubOne = $("<p class='card-text'>").text("Publisher: " + publisher);
       var sourceOne = $("<a>").attr("href", sourceURL);
       sourceOne.text("Source");
       var imageOne = $("<img class='card-img-top'>").attr("src", imgURL);
-      var buttoneOne = $("<i class = 'fa fa-thumbs-up '>").addClass(
-        "btn btn-primary"
+      var recipeIdOne = $("<p class='card-text'>").text("ID: " + recipeId);
+      var buttonLike = $("<button class='btn btn-primary'>").on(
+        "click",
+        function() {
+          event.preventDefault();
+          var query =
+            "https://www.food2fork.com/api/get?key=" +
+            key +
+            "&rId=" +
+            test.recipes[i].recipe_id;
+          $.ajax({
+            url: query,
+            method: "GET"
+          }).then(function(response) {
+            var likeSearch = JSON.parse(response);
+            console.log(likeSearch);
+          });
+        }
       );
-      var buttonTwo = $("<i class = 'fa fa-thumbtack'>").addClass(
-        "btn btn-danger"
+      var buttonPin = $("<button class='btn btn-danger'>").on(
+        "click",
+        function() {
+          event.preventDefault();
+          var query2 =
+            "https://www.food2fork.com/api/get?key=" +
+            key +
+            "&rId=" +
+            test.recipes[i].recipe_id;
+          $.ajax({
+            url: query2,
+            method: "GET"
+          }).then(function(response) {
+            var pinSearch = JSON.parse(response);
+            console.log(pinSearch);
+          });
+        }
       );
-      // <a href="#" class="btn btn-primary" id = "thumbs-up"><i class="fa fa-thumbs-up"></i></a>
       recipeDiv.append(titleOne);
       recipeDiv.append(sourceOne);
       recipeDiv.append(pubOne);
       recipeDiv.append(imageOne);
-      //   recipeDiv.append(buttoneOne);
-      //   recipeDiv.append(buttonTwo);
+      recipeDiv.append(recipeIdOne);
+      recipeDiv.append(buttonLike);
+      recipeDiv.append(buttonPin);
 
       var check = $("#result-box-row").append(recipeDiv);
       $("#result-box").append(check);
