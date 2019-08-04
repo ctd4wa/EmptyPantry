2bd03e5ce4a3fe7e1a0533ec5c6d81964136e8d3
Marine809 <rthorpesr@verizon.net>
2019-08-04 17:41:24 -0400

    changed model fields

--- models/svlater.js
+++ models/svlater.js
@@ -1,11 +1,10 @@
-console.log("Inside likes.js (Model)");
 module.exports = function(sequelize, DataTypes) {
-    var likes = sequelize.define("likes", {
+    var svlaters = sequelize.define("svlaters", {
         recieptTitle:  DataTypes.STRING,
         sourceUrl:     DataTypes.STRING,
         publisher:     DataTypes.STRING,
         recipeID:      DataTypes.STRING,
         userEmail:     DataTypes.STRING,
     });
-    return likes;
+    return svlaters;
   };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+      res.sendFile("main.html", {
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.sendFile("main.html", {
         example: dbExample
       });
     });
